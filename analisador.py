9d960e231aa56510d6dad43585691f80e260b34b
abeil-coelho <41490150+abeil-coelho@users.noreply.github.com>
2023-02-15 00:07:51 -0300

    Geração do relatório implementado.
    Aprimorado o salvamento de alinhamentos, bem como indicação de campos que utilizam vocabulário controlado.

--- analisador.py
+++ analisador.py
@@ -2,8 +2,9 @@ import pandas as pd
 import numpy as np
 import re
 import collections
+import os
 
-pd.options.mode.chained_assignment = None
+pasta_dados = "data"
 
 def verificador_multivalor(regex, valores, negativo, tipo):
     respostas = []
@@ -37,7 +38,7 @@ def verificador_multivalor(regex, valores, negativo, tipo):
         
     return resultado_final
 
-def verificador(regex, valor, negativo, tipo, nome_regra, museu, metadado):
+def verificador(regex, valor, negativo, tipo, nome_regra, museu, metadado, id):
     valor = str(valor)
     
     if valor == '<-99>':
@@ -45,8 +46,8 @@ def verificador(regex, valor, negativo, tipo, nome_regra, museu, metadado):
         return resposta
     else:
         if nome_regra == 'Fazer uso de vocabulário controlado':
-            controlados = pd.read_excel("./Campos_com_vocabularios_controlados.xlsx", index_col=0)
-            controlados_check = controlados.query("Colecao == '{}' and Campos_Ajutados == '{}'".format(museu, metadado))
+            controlados = pd.read_csv(os.path.join(id), index_col=0)
+            controlados_check = controlados.query("Campos_Ajutados == '{}'".format(metadado))
             if controlados_check.shape[0] == 0:
                 resposta = False
             else:
@@ -82,102 +83,3 @@ def verificador(regex, valor, negativo, tipo, nome_regra, museu, metadado):
         resposta = not resposta
 
     return resposta
-
-resultado_colecao = pd.DataFrame()
-
-for museu in museus:
-    tic()
-#     acervo_avaliado = acervo_ibram.query("Nome_Acervo == '{}'".format(museu))
-    acervo_avaliado = sampled_data.query("Nome_Acervo == '{}'".format(museu))
-    print("\n\n############################################\n")
-    print(museu)
-    print("A coleção possui", acervo_avaliado.shape[0],"itens.")
-    print("\n############################################\n\n")
-    
-
-    acervo_avaliado = acervo_avaliado.fillna("<-99>")
-    #acervo_avaliado["Creator"].replace({"Não identificado": "<-99>"}, inplace=True)
-    #acervo_avaliado["Creator"].replace({"Não identificada": "<-99>"}, inplace=True)
-    #acervo_avaliado["Creator"].replace({"Ignorado": "<-99>"}, inplace=True)
-    #acervo_avaliado["Creator"].replace({"Sem Autor": "<-99>"}, inplace=True)
-
-
-    
-    tamanho_total = acervo_avaliado.shape[0]
-
-    # Loop para cada elemento de metadado
-    for metadado in elementos_de_metadados:
-        print("\n\n",metadado)
-
-        # Selecionar regras aplicaveis ao elemento de metadado
-        regras_aplicaveis = regras_cco[regras_cco.iloc[:,1].str.contains(metadado)]
-
-        # Selecionar coluna correspondente ao metadado
-        coluna_foco = pd.DataFrame()
-        coluna_foco["foco"] = acervo_avaliado[metadado]
-
-        # Loop para cada regra existente
-        print("Quantidade de regras:",regras_aplicaveis.shape[0], "\n")
-        print("==========================================================================================")
-        
-        for index_regras, row_regras in regras_aplicaveis.iterrows():
-            nome_regra = row_regras.iloc[0]
-            regex = row_regras.iloc[2]
-            ind_negativo = row_regras.iloc[3]
-            tipo = row_regras.iloc[4]
-
-            avaliacoes = []
-
-            print("Nome da regra:",nome_regra,"\nExpressão:", regex,"\nNegação:", ind_negativo, "\nTipo:", tipo)
-
-            # Loop para cada registro presenta na coluna do metadado
-            for index_dado, row_dado in coluna_foco.iterrows():
-                dado_descricional = row_dado[0]            
-
-                avaliacao = verificador(regex, dado_descricional, ind_negativo, tipo, nome_regra, museu, metadado)
-                avaliacoes.append(avaliacao)
-
-            resultados_distintos = list(dict.fromkeys(avaliacoes))
-
-
-            counter=collections.Counter(avaliacoes)
-            print("Os resultados obtidos foram:",dict(counter),"\n")
-
-            resultado_geral = pd.DataFrame({'Avaliacao': avaliacoes, "Dado": coluna_foco["foco"], "Colecao": museu, "Campo_Metadado": metadado, "Regra": nome_regra, "Regex": regex, "Total": tamanho_total})
-            frames = [resultado_colecao, resultado_geral]
-            resultado_colecao = pd.concat(frames)
-
-            if len(resultados_distintos) == 1:
-                print("==> Amostra", resultados_distintos[0],"\n")
-                temp = resultado_geral.query("Avaliacao == {}".format(resultados_distintos[0]))
-                print(temp["Dado"].sample(),'\n')
-                
-            else:
-                print("==> Amostra", resultados_distintos[0],"\n")
-                temp = resultado_geral.query("Avaliacao == {}".format(resultados_distintos[0]))
-                print(temp["Dado"].sample(),'\n')
-
-                print("==> Amostra", resultados_distintos[1])                
-                temp = resultado_geral.query("Avaliacao == {}".format(resultados_distintos[1]))
-                print(temp["Dado"].sample(),'\n')
-
-            del resultado_geral, temp
-            print("=======================================")
-        del coluna_foco
-    tac("Competou a coleção", museu)
-    
-resultado_colecao = resultado_colecao.reset_index()
-resultado_colecoes_data = (resultado_colecao.groupby(["Colecao", "Regra", "Campo_Metadado", "Avaliacao", "Regex", "Total"]).agg(
-            resultado=("Avaliacao", "count")).reset_index())
-
-resultado_colecoes_data = resultado_colecoes_data.pivot(
-            index=["Colecao", "Campo_Metadado", "Regra", "Total"], columns=["Avaliacao"], values="resultado")
-resultado_colecoes_data = resultado_colecoes_data.reset_index()
-
-resultado_colecoes_data = resultado_colecoes_data.rename(columns={False: "0", True: "1"})
-resultado_colecoes_data = resultado_colecoes_data.fillna(0)
-resultados_preliminares = pd.merge(resultado_colecoes_data, dimencoes, how="left", on="Campo_Metadado")
-resultados_preliminares = resultados_preliminares[["Colecao", "Dimensão", "Campo_Metadado", "Regra", "0", "1", "Total"]]
-resultados_preliminares = resultados_preliminares.sort_values(by=["Dimensão", "Campo_Metadado"]).reset_index(drop=True)
-nome_arquivo = session['nome_rec'] + ".xlsx"
-resultados_preliminares.to_excel(os.path.join(pasta_dados, nome_arquivo))
--- app.py
+++ app.py
@@ -13,13 +13,14 @@ import csv
 import os
 import magic
 import json
-import analisador
+from analisador import verificador
 
 app = Flask(__name__)
 app.secret_key = "1skrLdKMnX'dZ{0#XEuS+r"
 app.config["UPLOAD_EXTENSIONS"] = ['.csv', '.CSV']
 app.config["UPLOAD_PATH"] = "uploads"
 pasta_dados = "data"
+arquivo_crosswalks = os.path.join(pasta_dados, "alinhamentos\\alinhamentos.parquet")
 
 esquema_cco = ['Work Type', 
 'Title', 
@@ -42,13 +43,12 @@ esquema_cco = ['Work Type',
 'Inscription', 
 'Location']
 
-
-# To do
-# 1. Recuperar alinhamento salvo por usuário
-# 2. Plotar alinhamento para edição (Editar)
-# 3. Aproveitar alinhamento (Salvar)
-# 4. Plugar projeto de avaliação no novo projeto
-
+try:
+    alinhamentos = pd.read_parquet(arquivo_crosswalks)
+except:
+    data = [["0", "0", 0]]
+    alinhamentos = pd.DataFrame(data, columns = ['nome', 'colunas', 'id'])
+    alinhamentos.to_parquet(arquivo_crosswalks)
 
 @app.route("/", methods=["GET"])
 def index():
@@ -109,54 +109,83 @@ def alinhamento():
     if request.method == "GET":
 
         dados_alinhamento = pd.read_csv(session["caminho_arquivo"], sep=session["delimitador"], encoding=session["encoding"], nrows=2)
+        dados_alinhamento = dados_alinhamento.loc[:,~dados_alinhamento.columns.str.match("Unnamed")]
         cabecalho_usuario_lista = dados_alinhamento.columns.tolist()
 
         # Salvar o cabeçalho do arquivo na seção 
         session["cabecalho_usuario_lista"] = cabecalho_usuario_lista
 
+        cabecalho_usuario = {k: v for v, k in enumerate(cabecalho_usuario_lista)}
+
         # Verificar se crosswalk já existe
         key_cabecalho_usuario = ''.join(cabecalho_usuario_lista)
         key_cabecalho_usuario.replace(" ", "_")
         
-        lista_pretendentes_croswalk = []
-        for file in os.listdir(os.path.join(pasta_dados, "alinhamentos")):
-            if file.startswith(key_cabecalho_usuario):
-                lista_pretendentes_croswalk.append(file)
-
-        if len(lista_pretendentes_croswalk) > 0:
-            #print(lista_pretendentes_croswalk)
-
-            lista_pretendentes_croswalk_clean = []
-            for item in lista_pretendentes_croswalk:
-                item = item.replace(key_cabecalho_usuario, "")
-                item = item.replace(".txt", "")
-                lista_pretendentes_croswalk_clean.append(item)
-
-            # Lista de pretendents com caminho completo e nome limpo
-            lista_pretendentes_croswalk = dict(map(lambda i,j : (i,j) , lista_pretendentes_croswalk_clean,lista_pretendentes_croswalk))
         
-        cabecalho_usuario = {k: v for v, k in enumerate(cabecalho_usuario_lista)}
-        return render_template("alinhamento.html", cabecalho_usuario=cabecalho_usuario, file_name=session['nome_arquivo'], esquema_cco=esquema_cco, lista_pretendentes_croswalk=lista_pretendentes_croswalk)
+        try:
+            lista_pretendentes_crosswalk = pd.read_parquet(arquivo_crosswalks)
+            lista_pretendentes_crosswalk = lista_pretendentes_crosswalk.query("colunas == '{}'".format(key_cabecalho_usuario))
+            lista_pretendentes_crosswalk = lista_pretendentes_crosswalk.set_index('nome').to_dict()['id']
+        
+        except:
+            lista_pretendentes_crosswalk = []
+        
+
+        return render_template("alinhamento.html", cabecalho_usuario=cabecalho_usuario, file_name=session['nome_arquivo'], esquema_cco=esquema_cco, lista_pretendentes_crosswalk=lista_pretendentes_crosswalk)
     
     if request.method == "POST":
         novo_crosswalk = request.form.items()
         crosswalk = []
+        crosswalk_vocabulario = []
         
         for indice, valor in novo_crosswalk:
-            crosswalk.append(valor)
+            if "vocabulario_controlado" in indice:
+                crosswalk_vocabulario.append(valor)
+            else:
+                crosswalk.append(valor)
+
+
         
         key_cabecalho_usuario = ''.join(session["cabecalho_usuario_lista"])
         key_cabecalho_usuario.replace(" ", "_")
 
-        nome_crosswalk = key_cabecalho_usuario + crosswalk[0] + ".txt"
+        nome_crosswalk = crosswalk[0]
+
+        
+        # Remover crosswalk com mesmo nome e colunas
+        croswalks_salvos = pd.read_parquet(arquivo_crosswalks)
+
+        croswalks_salvos = croswalks_salvos.query("colunas != '{}' or nome != '{}'".format(key_cabecalho_usuario, nome_crosswalk))
+
+        novo_id = int(croswalks_salvos['id'].max()) + 1
+
+
+        novo_crosswalk = pd.DataFrame([[nome_crosswalk, key_cabecalho_usuario, novo_id]], columns=['nome','colunas','id'])
+
+        frames = [croswalks_salvos, novo_crosswalk]
+        croswalks_salvos = pd.concat(frames)
+        croswalks_salvos.to_parquet(arquivo_crosswalks)
+
+        #Remover título do alinhamento
         crosswalk.pop(0)
 
         # Criar dicionário com cabeçalho antigo e novo
         crosswalk = dict(map(lambda i,j : (i,j) , session["cabecalho_usuario_lista"], crosswalk))
+
+
+        print("crosswalk_vocabulario_antes:", crosswalk_vocabulario)
+        crosswalk_vocabulario = list(map(lambda x: crosswalk.get(x, x), crosswalk_vocabulario))
+        print("crosswalk_vocabulario_depois:", crosswalk_vocabulario)
         
-        caminho_crosswalk = os.path.join(pasta_dados,"alinhamentos", nome_crosswalk)
+        # Salvar alinhamento com nome de colunas antes e depois
+        caminho_crosswalk = os.path.join(pasta_dados,"alinhamentos", str(novo_id))
         with open(caminho_crosswalk, 'w') as arquivo_crosswalk:
             arquivo_crosswalk.write(json.dumps(crosswalk))
+
+        # Salvar colunas com indicação de usu de vocabulário controlado
+        caminho_crosswalk_vocabulario = os.path.join(pasta_dados,"alinhamentos", str(novo_id)+"_vocabulario" )
+        crosswalk_vocabulario = pd.DataFrame(crosswalk_vocabulario, columns = ['Campos_Ajutados'])
+        crosswalk_vocabulario.to_csv(caminho_crosswalk_vocabulario)
 
         session["caminho_crosswalk"] = caminho_crosswalk
 
@@ -164,53 +193,139 @@ def alinhamento():
 
 @app.route("/recuperar_alinhamento", methods=["POST"])
 def recuperar_alinhamento():
-    ind_rec = request.form.get('ind_rec', None)
-    recuperacao = request.form.get('recuperacao', None)
-    nome_rec = request.form.get('nome_rec', None)
-    session['nome_rec'] = nome_rec
-
     if request.method == "POST":
+        ind_rec = request.form.get('ind_rec', None)
+        recuperacao = request.form.get('recuperacao', None)
+        nome_rec = request.form.get('nome_rec', None)    
 
         if ind_rec == "2":
             recuperacao = recuperacao
-            caminho_crosswalk = os.path.join(pasta_dados, "alinhamentos", recuperacao)
 
+            caminho_crosswalk = os.path.join(pasta_dados, "alinhamentos", recuperacao)
             with open(caminho_crosswalk) as crosswalk_salvo:
                 crosswalk_salvo = crosswalk_salvo.read()
+            print("crosswalk_salvo:", crosswalk_salvo)
             crosswalk_salvo = json.loads(crosswalk_salvo)
 
-            return render_template("editar_alinhamento.html", esquema_cco=esquema_cco, cabecalho_usuario=crosswalk_salvo, nome_crosswalk=nome_rec)
+            recuperacao = recuperacao + "_vocabulario"
+            recuperacao = os.path.join(pasta_dados, "alinhamentos", recuperacao)
+            caminho_crosswalk_vocabulario = pd.read_csv(recuperacao, index_col=0)
+            caminho_crosswalk_vocabulario = caminho_crosswalk_vocabulario['Campos_Ajutados'].tolist()
 
+            return render_template("editar_alinhamento.html", esquema_cco=esquema_cco, cabecalho_usuario=crosswalk_salvo, nome_crosswalk=nome_rec, caminho_crosswalk_vocabulario=caminho_crosswalk_vocabulario)
+
+        # Remover alinhamento
         if ind_rec == "3":
             recuperacao = recuperacao
+            # Exluir arquivo
             caminho_crosswalk = os.path.join(pasta_dados, "alinhamentos", recuperacao)
             os.remove(caminho_crosswalk)
+            
+            recuperacao = recuperacao + "_vocabulario"
+            caminho_crosswalk = os.path.join(pasta_dados, "alinhamentos", recuperacao)
+            os.remove(caminho_crosswalk)
+
+            # Remove registro
+            croswalks_salvos = pd.read_parquet(arquivo_crosswalks)
+            croswalks_salvos = croswalks_salvos.query("nome != '{}'".format(nome_rec))
+            croswalks_salvos.to_parquet(arquivo_crosswalks)
 
             return redirect(url_for('alinhamento'))
 
 
 @app.route("/processamento", methods=["GET", "POST"])
 def processamento():
+    # Carregando dados para processamento
+    regras_cco = pd.read_excel(os.path.join(pasta_dados, "fontes\\Base_Regex.xlsx"))
+    dimencoes = pd.read_excel(os.path.join(pasta_dados, "fontes\\Dimencoes.xlsx"))
 
+    # Carregando arquivo com o crosswalk
     with open(session["caminho_crosswalk"]) as crosswalk_recuperado:
         crosswalk_recuperado = crosswalk_recuperado.read()
     crosswalk_recuperado = json.loads(crosswalk_recuperado)
 
+    id = session["caminho_crosswalk"] + "_vocabulario"
+
+    cco_crosswalked = list(crosswalk_recuperado.values())
+    cco_crosswalked = list(set(cco_crosswalked))
+    cco_crosswalked.remove('Não utilizar')
+
+    resultado_colecao = pd.DataFrame()
+
+    # Lendo dados do usuário
     avaliacao_dados = pd.read_csv(session["caminho_arquivo"], sep=session["delimitador"], encoding=session["encoding"])
+    avaliacao_dados = avaliacao_dados.loc[:,~avaliacao_dados.columns.str.match("Unnamed")]
     avaliacao_dados.rename(columns=crosswalk_recuperado, inplace=True)
 
-    # Remover dados não utilizados
+    # Tratando dados do usuário
     avaliacao_dados = avaliacao_dados.loc[:, ~avaliacao_dados.columns.str.startswith('Não utilizar')]
+    avaliacao_dados = avaliacao_dados.fillna("<-99>")
+    tamanho_total = avaliacao_dados.shape[0]
 
-    regras_cco = pd.read_excel(os.path.join(pasta_dados, "fontes\\Base_Regex.xlsx"))
-    dimencoes = pd.read_excel(os.path.join(pasta_dados, "fontes\\Dimencoes.xlsx"))
-    controlados = pd.read_excel(os.path.join(pasta_dados, "fontes\\Campos_com_vocabularios_controlados.xlsx"), index_col=0)
+    # Iniciando loops
+    for metadado in cco_crosswalked:
+        print("\n\n", metadado)
 
-    nome_arquivo = session['nome_rec'] + ".xlsx"
+        # Selecionar regras aplicaveis ao elemento de metadado
+        regras_aplicaveis = regras_cco[regras_cco.iloc[:,1].str.contains(metadado)]
+
+        # Selecionar coluna correspondente ao metadado
+        coluna_foco = pd.DataFrame()
+        coluna_foco["foco"] = avaliacao_dados[metadado]
+
+        for index_regras, row_regras in regras_aplicaveis.iterrows():
+            nome_regra = row_regras.iloc[0]
+            regex = row_regras.iloc[2]
+            ind_negativo = row_regras.iloc[3]
+            tipo = row_regras.iloc[4]
+
+            avaliacoes = []
+
+            print("Nome da regra:",nome_regra,"\nExpressão:", regex,"\nNegação:", ind_negativo, "\nTipo:", tipo)
+
+            # Loop para cada registro presenta na coluna do metadado
+            for index_dado, row_dado in coluna_foco.iterrows():
+                dado_descricional = row_dado[0]            
+
+                avaliacao = verificador(regex, dado_descricional, ind_negativo, tipo, nome_regra, session["nome_arquivo"], metadado, id)
+                avaliacoes.append(avaliacao)
+
+            resultado_geral = pd.DataFrame({'Avaliacao': avaliacoes, "Dado": coluna_foco["foco"], "Colecao": session["nome_arquivo"], "Campo_Metadado": metadado, "Regra": nome_regra, "Regex": regex, "Total": tamanho_total})
+            frames = [resultado_colecao, resultado_geral]
+            resultado_colecao = pd.concat(frames)
+
+        del coluna_foco
+
+    resultado_colecao = resultado_colecao.reset_index()
+    resultado_colecoes_data = (resultado_colecao.groupby(["Colecao", "Regra", "Campo_Metadado", "Avaliacao", "Regex", "Total"]).agg(
+                resultado=("Avaliacao", "count")).reset_index())
+
+    resultado_colecoes_data = resultado_colecoes_data.pivot(
+                index=["Colecao", "Campo_Metadado", "Regra", "Total"], columns=["Avaliacao"], values="resultado")
+    resultado_colecoes_data = resultado_colecoes_data.reset_index()
+
+    resultado_colecoes_data = resultado_colecoes_data.rename(columns={False: "0", True: "1"})
+    resultado_colecoes_data = resultado_colecoes_data.fillna(0)
+    resultados_preliminares = pd.merge(resultado_colecoes_data, dimencoes, how="left", on="Campo_Metadado")
+    resultados_preliminares = resultados_preliminares[["Colecao", "Dimensão", "Campo_Metadado", "Regra", "0", "1", "Total"]]
+    resultados_preliminares = resultados_preliminares.sort_values(by=["Dimensão", "Campo_Metadado"]).reset_index(drop=True)
+
+    nome_arquivo = session["nome_arquivo"] + ".xlsx"
     nome_arquivo = os.path.join(pasta_dados, nome_arquivo)
+    session['nome_arquivo'] = nome_arquivo
 
-    return send_file(nome_arquivo, as_attachment=True)
+    resultados_preliminares.to_excel(nome_arquivo)
+    imagem = 2
+
+    return render_template("report.html", imagem=imagem)
+
+@app.route("/download", methods=["GET"])
+def download():
+
+    arquivo = session['nome_arquivo']
+
+    return send_file(arquivo, as_attachment=True)
 
 if __name__ == "__main__":
-    #app.run(host="192.168.100.38", port=8000, debug=True)
-    app.run(host="10.150.109.149", port=8000, debug=True)
+    app.run(host="192.168.100.38", port=8000, debug=True)
+    #app.run(host="10.150.109.149", port=8000, debug=True)
